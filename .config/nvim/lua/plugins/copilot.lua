d3757dfe5e8e878eead4eeabb80a546643743cde
joyoo9442 <joyoo9442@gmail.com>
2024-05-24 19:27:03 -0700

    configured copilot for nvim-cmp

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -54,10 +54,4 @@ return {
             })
         end,
     },
-    {
-        "zbirenbaum/copilot-cmp",
-        config = function ()
-            require("copilot_cmp").setup()
-        end,
-    }
 }
---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            {
+                "zbirenbaum/copilot-cmp",
+                config = function()
+                    require("copilot_cmp").setup()
+                end,
+            },
+        },
+        opts = {
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "copilot" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "nvim_lua" },
+                { name = "path" },
+            }
+        }
+    },
+}
